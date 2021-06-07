86444532dfdc7c5ccbf2b469eac1fb541f04509f
Viktoriia <viktoria.klimenko@ukr.net>
2021-06-07 15:13:36 +0300

    Cleaning code.

--- PastriesDelivery/Managers/DataValidator.cs
+++ PastriesDelivery/Managers/DataValidator.cs
@@ -5,7 +5,7 @@ namespace PastriesDelivery
     public static class DataValidator
     {
         private static string addresses { get; } = @"(?:улица|ул\.?)\s?[А-Я][а-я]*\.?\,?\s(?:дом|д\.?)\s?\d{2}(?:\,\s?(?:квартира|кв\.?)\s?\d{2}|)$";
-        private static string phoneNumbers { get; } = @"/\+?3?8?(0[\s\.-]?\(?\d{2}\)?[\s\.-]?\d{3}[\s\.-]?\d{2}[\s\.-]?\d{2})/gm";
+        private static string phoneNumbers { get; } = @"\+?3?8?(0[\s\.-]?\(?\d{2}\)?[\s\.-]?\d{3}[\s\.-]?\d{2}[\s\.-]?\d{2})";
 
         public static bool IsAddressValid(string address)
         {
