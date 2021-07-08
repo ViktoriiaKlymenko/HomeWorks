2a44fc81e585f4fee968a73edd33625c38919eac
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-07-08 16:40:38 +0300

    Function was added.

--- SQL/FourthTask/StoredProcedureScript.sql
+++ SQL/FourthTask/StoredProcedureScript.sql
@@ -1,2 +1,7 @@
-EXEC [AddNewId] 'RegionName', 'RegionDescription'
+DECLARE @Id nchar(50)
+EXECUTE [AddNewId] @Id, 'Regions'
 
+INSERT INTO [dbo].[Regions] 
+           ([Id], [Name], [Description])
+     VALUES
+           (@Id, 'Region name', 'Region description')
