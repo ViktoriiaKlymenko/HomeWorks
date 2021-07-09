1206be1813fe760239ed5113018ac4afc2721802
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-07-09 16:20:41 +0300

    New region and territory were added, query was changed.

--- SQL/ThirdTask/4.sql
+++ SQL/ThirdTask/4.sql
@@ -1,5 +1,12 @@
+USE [Northwind]
+GO
+
 SELECT [Region].RegionID, [Region].RegionDescription 
 FROM [dbo].[Territories]
-JOIN [dbo].[Region]  ON [Region].RegionID = [Territories].RegionID
-JOIN [dbo].[EmployeeTerritories] ON [Territories].TerritoryID = [EmployeeTerritories].TerritoryID
-WHERE [Territories].TerritoryID IS NULL;
+INNER JOIN [dbo].[EmployeeTerritories] ON [Territories].TerritoryID = [EmployeeTerritories].TerritoryID
+RIGHT JOIN [dbo].[Region]  ON [Region].RegionID = [Territories].RegionID
+WHERE [Territories].TerritoryID IS NULL; 
+
+GO
+
+
