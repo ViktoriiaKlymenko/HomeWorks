4a58d967222230f53090404c87b4422ff4a14079
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-07-20 12:46:27 +0300

    Classes for business-model were created.

--- PastriesDelivery/Models/Pastry.cs
+++ PastriesDelivery/Models/Pastry.cs
@@ -7,7 +7,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; }
+        public Category Category { get; set; }
         public int Weight { get; set; }
         public decimal Price { get; set; }
         public int Amount { get; set; }
---
+++ ThirdModule/Models/Provider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdModule
+{
+    public class Provider
+    {
+        public Guid Id { get; set; }
+        public ProviderType Type { get; set; }
+        public string Name { get; set; }
+    }
+
+}
