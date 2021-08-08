d48db0b7e1bb18b81d43f13143370797a6db1692
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-08-08 14:03:41 +0300

    Fixes were made for models.

--- ThirdModule/Models/Order.cs
+++ ThirdModule/Models/Order.cs
@@ -8,13 +8,16 @@ namespace ThirdModule
 {
     public class Order
     {
+        public Guid ClientId { get; set; }
         public Client Client { get; set; }
         public string DeliveryAdress { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTime OrderTime { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime DeliveryTime { get; set; }
+        public Guid ProviderId { get; set; }
         public Provider Provider { get; set; }
+        public Guid CourierId { get; set; }
         public Courier Courier { get; set; }
         public decimal DeliveryPrice { get; set; }
     }
--- ThirdModule/Models/Product.cs
+++ ThirdModule/Models/Product.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace ThirdModule
 {
-    class Product
+    public class Product
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public Guid CategoryId { get; set; }
         public Category Category { get; set; }
     }
 }      
