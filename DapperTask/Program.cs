405d9029234f9e41621392765f56a78cb5bce7bd
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-08-22 12:41:31 +0300

    Third task with dapper was started.

---
+++ DapperTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using DapperTask.Repo;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace DapperTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var configuration = Initialize();
+
+            var repository = new Repository(configuration.GetConnectionString("ConnectionString"));
+
+            var some = repository.GetProductById(1);
+            Console.WriteLine(some);
+            Console.ReadKey();
+        }
+        private static IConfiguration Initialize()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            return builder.Build();
+        }
+    }
+}
