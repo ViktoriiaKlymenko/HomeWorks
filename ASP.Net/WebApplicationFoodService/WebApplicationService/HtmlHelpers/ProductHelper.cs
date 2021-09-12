384a81c70c59cccd5f8c6b50816af20bf2104b75
Viktoriia <50338071+ViktoriiaKlymenko@users.noreply.github.com>
2021-09-12 19:18:31 +0300

    HtmlHelper was created.

---
+++ ASP.Net/WebApplicationFoodService/WebApplicationService/HtmlHelpers/ProductHelper.cs
@@ -0,0 +1,29 @@
+﻿using EntityFrameworkTask;
+using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationFoodService.HtmlHelpers
+{
+    public class ProductHelper
+    {
+        public static HtmlString GetProductList(IEnumerable<Product> products)
+        {
+            var result = "<ul>";
+            foreach (var product in products)
+            {
+                result = $"{result}<li>{product.Id}</li>" +
+                    $"<li>{product.Name}</li>" +
+                    $"<li>{product.Price}</li>" +
+                    $"<li>{product.Amount}</li>" +
+                    $"<li>{product.Weight}</li>" +
+                    $"<li>{product.Category.Name}</li>" +
+                    $"<li>{product.Provider.Name}</li>";
+            }
+            result = $"{result}</ul>";
+            return new HtmlString(result);
+        }
+    }
+}
